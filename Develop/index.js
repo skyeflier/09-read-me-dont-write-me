db485290e0e5b5e8354d170392e2cfa668ab3734
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-02-12 18:19:15 -0600

    created the functions in index.js to write the read me

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,47 @@
+// THIS FILE is where the questions would be asked, and will create the entire string of answers
+
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer'); // It knows what it's called from the package.json file
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input', // LOOK AT PROMPT TYPES IN INQUIRER SITE
+    message: 'What is the title of your Read Me?',
+    name: 'title',
+},
+{
+    type: 'password',
+    message: 'What is your password?',
+    name: 'password',
+},
+{
+    type: 'list',
+    choices: ['MIT', 'Adobe', 'none'], //ADD MORE LICENSE TYPES!  
+    message: 'What is the Markdown License for your project?',
+    name: 'markdownLicense',
+},
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file - THEN CALL THIS FUNCTION TO CREATE A README FILE
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data, err => { // If you want to include variables, you need to use back ticks `` for the string, and the ${} so it can dynamically pull data 
+        if (err) {
+            console.error(err);
+        }
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions) //Prompt is a method inside inquirer
+        .then((response) => {// PASS GO GENERATE MARKDOWN THEN IN GM YOU CAN USE THIS OBJECT TO GET ALL THE DATA YOU NEED
+            response.confirm === response.password
+                ? console.log('Success!')
+                : console.log('You forgot your password already?!')
+            writeToFile('README.md', generateMarkdown(response));
+        });
+}
 
-// Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,26 @@
+
+// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// If there is no license, return an empty string //LOOK UP HOW TO CREATE A LICENSE BADGE  // USE INQUIRER TO GIVE OPTIONS 
+function renderLicenseBadge(license) { 
+  // if none return empty string else {return line 8}
+return `I AM THE LICENSE ${license}` // THIS IS WHERE I ACCESS 
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
-// TODO: Create a function to generate markdown for README
+// TODO: Create a function to generate markdown for README - FIGURE OUT QUSTIONS AND ADD THEM HERE
 function generateMarkdown(data) {
   return `# ${data.title}
-
+# ${renderLicenseBadge(data.markdownLicense)}
 `;
 }
 

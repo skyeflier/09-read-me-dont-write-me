873798fd4d56dd831b4ce8b6536bcb3f3bb42b62
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-02-14 17:47:09 -0600

    added table of contents

--- Develop/README.md
+++ Develop/README.md
@@ -1,12 +1,36 @@
-# title
-          # des
-          # inst
-          # bob
-          # us
-          # them
+y
+          License Badge: ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+          
+          ## Description: 
+          y
+  ## Table of Contents
+  [Title](##-y)
+  [Description](##-description)
+  [Installation Instructions](##-installation)
+  [Usage Information](##-usage)
+  [License](##-license)
+  [Contributors](##-contributors)
+  [Test Information](##-tests)
+  [Questions](##-questions)
+          
+          ## Installation: 
+          y
+          
+          ## Usage: 
+          y
+          
+          ## Contributors: 
+          y
 
-  # 
-  # 
-  # 
+          ## Tests: 
+          y
+
+          ## License: 
+          undefined
+          License Link: [License: MIT](https://opensource.org/licenses/MIT)
+         
+          ## Questions
+          Find me on GitHub here: y
+          Contact me via email here: y
 
   
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,33 +1,35 @@
 // TODO: Create a function that returns a license badge based on which license is passed in. If there is no license, return an empty string //LOOK UP HOW TO CREATE A LICENSE BADGE  // USE INQUIRER TO GIVE OPTIONS 
 function renderLicenseBadge(license) {
+  let markdownLicenseBadge = ""
   if (license === 'none') {
-    return "";
+    return markdownLicenseBadge;
   } else {
     if (license === 'MIT') {
-      markdownLicense = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+      markdownLicenseBadge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
     } else if (license === 'GPLv3') {
-      markdownLicense = `[![GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`
+      markdownLicenseBadge = `![GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
     } else if (license === 'Apache') {
-      markdownLicense = `[![Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`
-      return `License: ${markdownLicense}`
+      markdownLicenseBadge = `![Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
     }
   }
+  return `License Badge: ${markdownLicenseBadge}`
 };
 
 // TODO: Create a function that returns the license link. If there is no license, return an empty string
 function renderLicenseLink(license) {
+  let markdownLicenseLink = ""
   if (license === 'none') {
-    return "";
+    return markdownLicenseLink;
   } else {
     if (license === 'MIT') {
-      markdownLicense = `[![License: MIT](https://opensource.org/licenses/MIT)]`
+      renderLicenseLink = `[License: MIT](https://opensource.org/licenses/MIT)`
     } else if (license === 'GPLv3') {
-      markdownLicense = `[![GPL v3](https://www.gnu.org/licenses/gpl-3.0)]`
+      renderLicenseLink = `[GPL v3](https://www.gnu.org/licenses/gpl-3.0)`
     } else if (license === 'Apache') {
-      markdownLicense = `[![Apache](https://opensource.org/licenses/Apache-2.0)]`
-      return `License: ${markdownLicense}`
+      renderLicenseLink = `[Apache](https://opensource.org/licenses/Apache-2.0)`
     }
   }
+  return `License Link: ${renderLicenseLink}`
 }
 
 // TODO: Create a function that returns the license section of README. If there is no license, return an empty string
@@ -40,21 +42,42 @@ function renderLicenseSection(license) {
   }
 }
 
-// TODO: Create a function to generate markdown for README - FIGURE OUT QUESTIONS AND ADD THEM HERE
+// This generates the markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-          # ${renderLicenseBadge(data.markdownLicense)}
-          # ${data.description}
-          # ${data.installation}
-          # ${data.usage}
-          # ${data.contributors}
-          # ${data.test}
+  return `${data.title}
+          ${renderLicenseBadge(data.markdownLicense)}
+          
+          ## Description: 
+          ${data.description}
+  ## Table of Contents
+  [Title](##-${data.title})
+  [Description](##-description)
+  [Installation Instructions](##-installation)
+  [Usage Information](##-usage)
+  [License](##-license)
+  [Contributors](##-contributors)
+  [Test Information](##-tests)
+  [Questions](##-questions)
+          
+          ## Installation: 
+          ${data.installation}
+          
+          ## Usage: 
+          ${data.usage}
+          
+          ## Contributors: 
+          ${data.contributors}
 
-          # ${renderLicenseSection(data.markdownLicense)}
-          # ${renderLicenseLink(data.markdownLicense)}
+          ## Tests: 
+          ${data.test}
 
-          # ${data.githubUsername}
-          # ${data.emailAddress}
+          ## License: 
+          ${renderLicenseSection(data.markdownLicense)}
+          ${renderLicenseLink(data.markdownLicense)}
+         
+          ## Questions
+          Find me on GitHub here: ${data.githubUsername}
+          Contact me via email here: ${data.emailAddress}
 
   `;
 }
